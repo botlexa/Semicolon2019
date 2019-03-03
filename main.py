ab01c9639d051c51995bca5c49d447faa70f9430
botlexa <pankaj2019semicolon@gmail.com>
2019-03-03 15:07:08 +0530

    added procfile

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template ,redirect,request,url_for,session
+from flask import Flask, render_template, redirect, request, url_for, session
 import os
 import re
 import psycopg2
+
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
@@ -10,13 +11,14 @@ app.secret_key = os.urandom(24)
 def login():
     return render_template('login.html')
 
-@app.route('/results' ,methods=['POST'])
+
+@app.route('/results', methods=['POST'])
 def result():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
 
-        cur=connection()
+        cur = connection()
         cur.execute("select username,password from usermaster")
         rows = cur.fetchall()
         for x in rows:
@@ -30,58 +32,62 @@ def result():
 def create_form():
     return render_template('create_form.html')
 
+
 @app.route('/register_form', methods=['POST'])
 def register_form():
     val = request.form
     cur = connection()
-    username=session['username']
-    query_user = "select userid from usermaster where username="+"'"+ username +"'"+";"
+    username = session['username']
+    query_user = "select userid from usermaster where username=" + "'" + username + "'" + ";"
     cur.execute(query_user)
     user_id = cur.fetchall()
-    query_form = "insert into templatemaster (templatename,description,userid)values ("+"'"+val['template_name']+"'"+","+"'"+val['description']+"'"+","+str(user_id[0][0])+");"
+    query_form = "insert into templatemaster (templatename,description,userid)values (" + "'" + val[
+        'template_name'] + "'" + "," + "'" + val['description'] + "'" + "," + str(user_id[0][0]) + ");"
 
     try:
         cur.execute(query_form)
     except Exception as e:
-        return "failed"+e
+        return "failed" + e
 
-    query_form_id="select templatemasterid from templatemaster where templatename="+"'"+val['template_name']+"'"+";"
+    query_form_id = "select templatemasterid from templatemaster where templatename=" + "'" + val[
+        'template_name'] + "'" + ";"
     cur.execute(query_form_id)
     form_id = cur.fetchall()
 
-
-
     for x in val:
         query_form_details = "insert into fieldmaster (fieldname,templatemasterid) values("
-        if re.search('^Field[0-9]+$',x):
-            query_form_details += "'"+ val[x] +"',"+str(form_id[0][0])+");"
+        if re.search('^Field[0-9]+$', x):
+            query_form_details += "'" + val[x] + "'," + str(form_id[0][0]) + ");"
             try:
                 cur.execute(query_form_details)
             except Exception as e:
                 return e
     return "success"
 
+
 @app.route('/edit_form')
 def edit_form():
     cur = connection()
-    query_userid="select userid from usermaster where username="+"'"+session['username']+"';"
+    query_userid = "select userid from usermaster where username=" + "'" + session['username'] + "';"
     cur.execute(query_userid)
     user_id = cur.fetchall()
-    query_form="select templatemasterid,templatename from templatemaster where userid="+str(user_id[0][0])+";"
+    query_form = "select templatemasterid,templatename from templatemaster where userid=" + str(user_id[0][0]) + ";"
     cur.execute(query_form)
     form_details = cur.fetchall()
-    return render_template('edit_form.html',form_details=form_details,flag="edit_form")
+    return render_template('edit_form.html', form_details=form_details, flag="edit_form")
+
 
 @app.route('/edit_form_data/<form_id>')
 def edit_form_data(form_id):
     cur = connection()
-    query = "select fieldname from fieldmaster where templatemasterid="+"'"+form_id+"'"+";"
+    query = "select fieldname from fieldmaster where templatemasterid=" + "'" + form_id + "'" + ";"
     cur.execute(query)
     field_name = cur.fetchall()
     # query_field_name = "select fieldname from where templatemasterid="+"'"+str(template_id)+"';"
     # cur.execute(query_field_name)
     # field_name = cur.fetchall()
-    return render_template('edit_form.html',field_name=field_name,flag="edit_form_data")
+    return render_template('edit_form.html', field_name=field_name, flag="edit_form_data")
+
 
 def connection():
     conn = psycopg2.connect(host='localhost', user='postgres', password='flipcart')
@@ -90,6 +96,5 @@ def connection():
     return cur
 
 
-
-if __name__=='__main__':
-    app.run(host="localhost", debug=True)
+if __name__ == '__main__':
+    app.run(host="localhost",port=8000, debug=True)
